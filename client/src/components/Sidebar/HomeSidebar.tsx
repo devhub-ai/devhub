5a3238fd4392a46b5c63c79c5461f34ef466bb7f
Deepraj <deepraj21.bera@gmail.com>
2024-07-31 02:36:29 +0530

    Update the Readme, contribution and installation guide

--- client/src/components/Sidebar/HomeSidebar.tsx
+++ client/src/components/Sidebar/HomeSidebar.tsx
@@ -111,7 +111,9 @@ export const Logo = () => {
             to="#"
             className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
         >
-            <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+            {/* <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" /> */}
+            <h1 style={{ fontSize: "1.3rem", fontWeight: "bold" }} className="font-medium text-black dark:text-white whitespace-pre"
+            >dh</h1>
             <motion.span
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
@@ -129,7 +131,8 @@ export const LogoIcon = () => {
             to="#"
             className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
         >
-            <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+            <h1 style={{ fontSize: "1.3rem", fontWeight: "bold" }} className="font-medium text-black dark:text-white whitespace-pre"
+            >dh</h1>
         </Link>
     );
 };
