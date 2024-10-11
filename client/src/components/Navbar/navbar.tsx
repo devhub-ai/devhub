2fc5f0999d8135f9eda2bdef8df2746a087fc858
Milos Spartan <milos.djurdjevic.brains24@gmail.com>
2024-10-11 16:07:15 +0200

    feat(landing-page): add necessary fixes to responsive landing page

--- client/src/components/Navbar/navbar.tsx
+++ client/src/components/Navbar/navbar.tsx
@@ -8,6 +8,7 @@ import {
   IconTerminal2,
 } from "@tabler/icons-react";
 import React from "react";
+import { ModeToggle } from "../Theme/mode-toggle";
 
 interface Link {
   title: string;
@@ -20,32 +21,32 @@ export function Navbar() {
     {
       title: "Home",
       icon: <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-      to: "#top", 
+      to: "#top",
     },
     {
       title: "Features",
       icon: <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-      to: "#features", 
+      to: "#features",
     },
     {
       title: "FAQs",
       icon: <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-      to: "#help", 
+      to: "#help",
     },
     {
       title: "DevHub",
       icon: <img src="../../../public/logo.png" width={20} height={20} alt="DevHub" />,
-      to: "#", 
+      to: "#",
     },
     {
       title: "DevMap",
       icon: <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-      to: "#", 
+      to: "#",
     },
     {
       title: "Discord",
       icon: <IconBrandDiscord className="h-full w-full text-neutral-500 dark:text-neutral-300" />,
-      to: "https://discord.gg/he8QHEC8WP", 
+      to: "https://discord.gg/he8QHEC8WP",
     },
     {
       title: "GitHub",
@@ -64,14 +65,29 @@ export function Navbar() {
     }
   };
 
+  const handleGithubClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    window.open("https://github.com/devhub-ai/devhub", "_blank");
+  }
+
   return (
     <div className="flex items-center justify-center mt-16 w-full">
-      <FloatingDock
-        items={links.map(link => ({
-          ...link,
-          onClick: (event: React.MouseEvent<HTMLAnchorElement>) => handleClick(event, link),
-        }))}
-      />
+      <a href="/"><img src="../../../public/logo.png" className="size-10 mx-5" /></a>
+      <div className="hidden md:block">
+        <FloatingDock
+          items={links.map(link => ({
+            ...link,
+            onClick: (event: React.MouseEvent<HTMLAnchorElement>) => handleClick(event, link),
+          }))}
+        />
+      </div>
+      <div className="flex gap-4 md:hidden">
+        <button onClick={(e) => handleClick(e, { to: "#features" })}><IconTerminal2 className="size-10" /></button>
+        <button onClick={(e) => handleGithubClick(e)}><IconBrandGithub className="size-10" /></button>
+      </div>
+      <div className="px-4">
+        <ModeToggle />
+      </div>
     </div>
   );
 }
